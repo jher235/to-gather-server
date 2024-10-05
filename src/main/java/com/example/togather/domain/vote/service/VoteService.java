d938bd924bfb49ee382836f5ba26dc65db642711
aaahyunseo <hs030608@naver.com>
2024-10-06 00:06:46 +0900

    feat/vote 수정

--- src/main/java/com/example/togather/domain/vote/service/VoteService.java
+++ src/main/java/com/example/togather/domain/vote/service/VoteService.java
@@ -4,7 +4,7 @@ import com.example.togather.domain.meeting.entity.Meeting;
 import com.example.togather.domain.meeting.repository.MeetingRepository;
 import com.example.togather.domain.place.entity.Place;
 import com.example.togather.domain.place.repository.PlaceRepository;
-import com.example.togather.domain.user.User;
+import com.example.togather.domain.user.entity.User;
 import com.example.togather.domain.user.repository.UserRepository;
 import com.example.togather.domain.vote.entity.Vote;
 import com.example.togather.domain.vote.repository.VoteRepository;
@@ -35,11 +35,11 @@ public class VoteService {
 
         for (VoteSelectDto voteSelectDto : voteSelectListDto.getVoteSelectDtoList()) {
             User user = userRepository.findByUserName(voteSelectDto.getUserName())
-                    .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_USER));
+                    .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
             Place place = placeRepository.findByPlaceId(voteSelectDto.getPlaceId())
                     .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_PLACE));
             Meeting meeting = meetingRepository.findById(voteSelectDto.getMeetingId())
-                    .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MEETING));
+                    .orElseThrow(() -> new NotFoundException(ErrorCode.MEETING_NOT_FOUND));
 
             Vote vote = new Vote(user, place, meeting);
             voteRepository.save(vote);  // 투표 데이터 저장
@@ -52,7 +52,7 @@ public class VoteService {
      **/
     public VoteResponseData getVoteResult(UUID meetingId){
         Meeting meeting = meetingRepository.findByMeetingId(meetingId)
-                .orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND_MEETING));
+                .orElseThrow(() -> new NotFoundException(ErrorCode.MEETING_NOT_FOUND));
         return VoteResponseData.from(meeting);
     }
 }
