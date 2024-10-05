9c6d83921327c0b839624d09a37cf6662a8588d4
jher235 <tim668666@gmail.com>
2024-10-05 23:05:05 +0900

    feat: 미팅으로 미팅에 해당하는 시간표 찾는 JPA메서드 작성

--- src/main/java/com/example/togather/domain/time/repository/TimeJpaRepository.java
+++ src/main/java/com/example/togather/domain/time/repository/TimeJpaRepository.java
@@ -1,9 +1,13 @@
 package com.example.togather.domain.time.repository;
 
-import com.example.togather.domain.time.Time;
+import com.example.togather.domain.meeting.entity.Meeting;
+import com.example.togather.domain.time.entity.Time;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TimeJpaRepository extends JpaRepository<Time, Long> {
+    public List<Time> findAllByMeeting(Meeting meeting);
 }
