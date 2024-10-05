b44c5f34cfdc12f18bc0eadabebceca0463d27eb
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2024-10-06 02:04:01 +0900

    FEAT: place info

--- src/main/java/com/example/togather/domain/place/entity/Place.java
+++ src/main/java/com/example/togather/domain/place/entity/Place.java
@@ -1,8 +1,12 @@
 package com.example.togather.domain.place.entity;
 
 import com.example.togather.domain.meeting.entity.Meeting;
+import com.example.togather.domain.vote.entity.Vote;
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import java.util.LinkedHashMap;
+import java.util.List;
 import lombok.Data;
 
 @Entity
@@ -17,4 +21,8 @@ public class Place {
     @JsonBackReference
     @JoinColumn(name = "meeting_id")
     private Meeting meeting;
+
+    @OneToMany(mappedBy = "place", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Vote> votes;
 }
--- src/main/java/com/example/togather/domain/place/repository/PlaceRepository.java
+++ src/main/java/com/example/togather/domain/place/repository/PlaceRepository.java
@@ -1,10 +1,15 @@
 package com.example.togather.domain.place.repository;
 
 import com.example.togather.domain.place.entity.Place;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
     Optional<Place> findByPlaceId(Long placeId);
+
+    Optional<List<Place>>findAllByMeetingMeetingId(UUID uuid);
+
 }
