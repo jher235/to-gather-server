af840f3281fefde97a65070acf4a9c264820c3c4
jher235 <tim668666@gmail.com>
2024-10-05 22:02:59 +0900

    feat: Time 엔티티 수정

--- src/main/java/com/example/togather/domain/time/Time.java
+++ src/main/java/com/example/togather/domain/time/Time.java
@@ -4,26 +4,57 @@ import com.example.togather.domain.meeting.Meeting;
 import com.example.togather.domain.user.User;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
 @Entity
-@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Time {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long timeId;
+
+    @Column(nullable = false)
     private Long blockId;
+
+    @Column(nullable = false)
     private boolean whether;
+
+    @Column(length = 1000)
     private String memo;
 
     @ManyToOne(optional = false)
     @JsonBackReference
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(optional = false)
     @JsonBackReference
-    @JoinColumn(name = "meeting_id")
+    @JoinColumn(name = "meeting_id", nullable = false)
     private Meeting meeting;
+
+    public Time(final Long blockId,
+                final boolean whether,
+                final String memo,
+                final User user,
+                final Meeting meeting){
+        this.blockId = blockId;
+        this.whether = whether;
+        this.memo = memo;
+        this.user = user;
+        this.meeting = meeting;
+    }
+
+    public Time(final Long blockId,
+                final boolean whether,
+                final User user,
+                final Meeting meeting){
+        this.blockId = blockId;
+        this.whether = whether;
+        this.memo = null;
+        this.user = user;
+        this.meeting = meeting;
+    }
+
 }
 
